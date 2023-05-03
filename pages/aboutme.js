cf5a8e47de7220869773092a53fd63ac62f73ca0
Eric Bilaver <ericbilaver@gmail.com>
2023-05-03 16:31:12 -0400

    rough about page

--- pages/aboutme.js
+++ pages/aboutme.js
@@ -5,11 +5,10 @@ import content from "../assets/content.json";
 export default function AboutMe() {
   return (
     <div className={css.aboutMe}>
-      <Link className={css.link} href="/">
-        Home
+      <Link className={css.nameTitle} href="/">
+        {content.aboutme.title}
       </Link>
-      <div className={css.aboutMe}>
-        <div className={css.nameTitle}>{content.aboutme.title}</div>
+      <div className={css.aboutMeContent}>
         <div className={css.description}>{content.aboutme.description}</div>
         <div className={css.talents}>
           <div className={css.talent}>
@@ -23,11 +22,16 @@ export default function AboutMe() {
               {content.aboutme.lists[1].name}
             </div>
             <a href="https://www.linkedin.com/in/ericbilaver/">LinkedIn</a>
+            <br />
             <a href="https://www.instagram.com/ericbilaver/">Instagram</a>
+            <br />
             <a href="https://github.com/ericbilaver">GitHub</a>
+            <br />
             <a href="">Resume</a>
           </div>
         </div>
+        <br />
+        <br />
       </div>
     </div>
   );
