d5be3f88a6cbe4549f121011e17e1aa921ffae82
Eric Bilaver <ericbilaver@gmail.com>
2023-05-09 17:04:18 -0400

    fixed fonts and work page

--- components/WorkItem/WorkItem.js
+++ components/WorkItem/WorkItem.js
@@ -1,10 +1,27 @@
+import Image from "next/image";
 import css from "./WorkItem.module.scss";
+import Link from "next/link";
 
-export default function WorkItem({ data }) {
+export default function WorkItem({ data, videoPath, logoPath }) {
   return (
-    <div className={css.workItem}>
-      <h1 className={css.title}>{data.title}</h1>
-      <h3 className={css.company}>{data.company}</h3>
+    <div className={css.workItem} style={{ background: data.color }}>
+      <div className={css.eyebrowRow}>
+        {data.link === "" ? null : (
+          <div className={css.Image}>
+            <Link href={data.link}>
+              <Image src={data.logo} width="60" height="60" />
+            </Link>
+          </div>
+        )}
+        <div className={css.eyebrowText}>
+          <h2>
+            {data.title} - {data.company}
+          </h2>
+          <p>{data.location}</p>
+        </div>
+      </div>
+
+      <div className={css.accomplishments}>{data.desc.join("\n")}</div>
     </div>
   );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import './index.scss'
+import Head from "next/head";
+import "./index.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <style>
+          @import
+          url('https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600&display=swap');
+        </style>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
